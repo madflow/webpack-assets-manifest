0beed31a5d996c772a4b800c701cfd6be9a6544c
madflow <madflow@web.de>
2020-10-19 16:23:33 +0200

    fix copy mock

--- test/mocks/CopyPluginMock.js
+++ test/mocks/CopyPluginMock.js
@@ -1,6 +1,6 @@
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-const { RawSource } = webpack.sources || require('webpack-sources');
+const { RawSource } = webpack.sources || require("webpack-sources");
 
 class CopyPluginMock {
   constructor(assets) {
@@ -10,15 +10,19 @@ class CopyPluginMock {
   apply(compiler) {
     const pluginName = this.constructor.name;
 
-    compiler.hooks.thisCompilation.tap(pluginName, compilation => {
+    compiler.hooks.thisCompilation.tap(pluginName, (compilation) => {
       compilation.hooks.additionalAssets.tapAsync(
         pluginName,
-        async callback => {
-          this.assets.forEach(asset => {
+        async (callback) => {
+          this.assets.forEach((asset) => {
             const { targetPath, data } = asset;
             const source = new RawSource(data);
 
-            compilation.emitAsset(targetPath, source, {});
+            if (typeof compilation.emitAsset !== "function") {
+              compilation.assets[targetPath] = source;
+            } else {
+              compilation.emitAsset(targetPath, source, {});
+            }
           });
           callback();
         }
