de2598db708bc518f7b1092f5a1621767ff2afb7
madflow <madflow@web.de>
2020-10-19 15:59:34 +0200

    output assets without a chunk name, should fix #49

---
+++ test/mocks/CopyPluginMock.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+
+const { RawSource } = webpack.sources || require('webpack-sources');
+
+class CopyPluginMock {
+  constructor(assets) {
+    this.assets = assets;
+  }
+
+  apply(compiler) {
+    const pluginName = this.constructor.name;
+
+    compiler.hooks.thisCompilation.tap(pluginName, compilation => {
+      compilation.hooks.additionalAssets.tapAsync(
+        pluginName,
+        async callback => {
+          this.assets.forEach(asset => {
+            const { targetPath, data } = asset;
+            const source = new RawSource(data);
+            if (typeof compilation.emitAsset !== "function") {
+              compilation.assets[targetPath] = source;
+            } else {
+              compilation.emitAsset(targetPath, source, {});
+            }
+          });
+          callback();
+        }
+      );
+    });
+  }
+}
+
+module.exports = CopyPluginMock;
